624024f84f3061e574d47697459245167c8d72e9
hikaMaeng <hika00@gmail.com>
2014-04-26 21:37:56 +0900

    로직변경중

--- bsSelector_op.js
+++ bsSelector_op.js
@@ -11,8 +11,9 @@ var bsSelector = function( doc, trim, detect, domData ){
 	var isQSA = doc['querySelectorAll'] ? 1 : 0, isClsName = doc['getElementsByClassName'] ? 1 : 0,
 		isIE = detect.browser == 'ie' ? 1 : 0, ieVer = detect.browserVer,
 		rTag = /^[a-z]+[0-9]*$/i, rAlpha = /[a-z]/i, rClsTagId = /^[.#]?[a-z0-9]+$/i,
-		tokenize, compare, query, hasParent, hasQSAErr, oSel;
-		
+		tokenize, compare, query, qtime, TIME, hasParent, hasQSAErr, oSel;
+	
+	
 	oSel = [],
 	tokenize = (function(trim){
 		var mParent = {' ':1, '>':1}, mQSAErr = '!', mBracket = {'[':0, '(':0, ']':1, ')':1},
@@ -43,21 +44,17 @@ var bsSelector = function( doc, trim, detect, domData ){
 			return tks;
 		};
 	})(trim),
+	qtime = {},
 	compare = (function(){
-		var r0 = /"|'/g, i, j, setIdx = function( pEl, dir, tagName ){
-			var els, el, tname, iname, t0, i, j = 1;
-			if( !pEl || pEl.tagName == 'HTML' ) return 0;
-			tname = 'DQtime' + ( tagName ? 'T' : '' ) + ( dir ? 'L' : '' ), iname = 'DQindex' + ( tagName ? 'T' : '' ) + ( dir ? 'L' : '' ),
-			els = pEl.childNodes, i = els.length;
-			while( i-- ){
-				el = els[i];
-				if( el.nodeType == 1 && ( !tagName || el.tagName == tagName ) ) ( t0 = domData(el) )[tname] = ( tagName || '' ) + finder.bsRtime, t0[iname] = dir ? i + 1 : j++;
-			}
-			return 1;
-		}, mT0 = {'~':1, '|':1, '!':1, '^':1, '$':1, '*':1};
+		var r0 = /"|'/g, i, j, 
+		mT0 = {'~':1, '|':1, '!':1, '^':1, '$':1, '*':1},
+		enabled = {INPUT:1, BUTTON:1, SELECT:1, OPTION:1, TEXTAREA:1},
+		checked = {INPUT:1, radio:1, checkbox:1, OPTION:2},
+		first = {'first-child':0, 'last-child':0, 'first-of-type':1, 'last-of-type':1},
+		times = {'nth-child':'DQt0', 'nth-last-child':'DQt1', 'first-child':'DQt2', 'last-child':'DQt3', 'first-of-type':'DQt4', 'last-of-type':'DQt5'},
+		indexes = {'nth-child':'DQi0', 'nth-last-child':'DQi1', 'first-child':'DQi2', 'last-child':'DQi3', 'first-of-type':'DQi4', 'last-of-type':'DQi5'};
 		return function( el, token ){
-			var k, v, t0, t1, i, j, elTagName, elIdx, pEl, tname, ename;
-
+			var data, time, indexes, parent, children, tag, update, dir, t0, t1, k, v, i, j, m;
 			switch( token.charAt(0) ){
 			case'#':return token.substr(1) == el.id;
 			case'.':return !( t0 = el.className ) ? 0 : ( k = token.substr(1), t0.indexOf(' ') > -1 ? k == t0 : t0.split(' ').indexOf(k) > -1 );
@@ -80,138 +77,92 @@ var bsSelector = function( doc, trim, detect, domData ){
 				if( v ) k = k.substring( 0, i );
 				switch( k ){
 				case'active':case'visited':case'first-line':case'first-letter':case'hover':case'focus':return;
-				case'link':return el.tagName.toUpperCase() == 'A' && el.getAttribute('href') !== null;
-				case'root':return el.tagName.toUpperCase() == 'HTML';
+				case'link':return el.tagName == 'A' && el.getAttribute('href');
+				case'root':return el.tagName == 'HTML';
 				case'empty':return el.nodeType == 1 && !el.nodeValue && !el.childNodes.length;
-				case'checked':
-					return elTagName = el.tagName, ( elTagName == 'INPUT' && (el.getAttribute('type') == 'radio' || el.getAttribute('type') == 'checkbox' ) && el.checked == true ) ||
-						( elTagName == 'OPTION' && el.selected == true );
-				case'enabled':
-					return elTagName = el.tagName, (elTagName == 'INPUT' || elTagName == 'BUTTON' || elTagName == 'SELECT' || elTagName == 'OPTION' || elTagName == 'TEXTAREA') &&
-						el.getAttribute('disabled') == null;
-				case'disabled':
-					return elTagName = el.tagName, (elTagName == 'INPUT' || elTagName == 'BUTTON' || elTagName == 'SELECT' || elTagName == 'OPTION' || elTagName == 'TEXTAREA') &&
-						el.getAttribute('disabled') != null
-						
-				case'first-of-type':case'last-of-type':
-					elTagName = el.tagName, pEl = el.parentNode, t0 = domData(el), 
-					key.charAt(0) == 'f' ? ( tname = 'DQtimeFCT', ename = 'DQFCTEl' ) : ( tname = 'DQtimeLCT', ename = 'DQLCTEl' );
-					if( !t0[tname] || t0[tname] != elTagName + finder.bsRtime ){
-						t0[tname] = elTagName + finder.bsRtime;
-						if( ( op = pEl && pEl.childNodes ) && ( i = op.length ) ){
-							t0[ename] = null;
-							if( key.charAt(0) == 'f' ){
-								for( j = i, i = 0 ; i < j ; i++ ){
-									if( op[i].nodeType == 1 && op[i].tagName == elTagName ){
-										t0[ename] = op[i];
-										break;
-									}
-								}
-							}else{
-								while( i-- ){
-									if( op[i].nodeType == 1 && op[i].tagName == elTagName ){
-										t0[ename] = op[i];
-										break;
-									}
-								}
+				case'checked':return t0 = checked[el.tagName], ( t0 == 1 && el.checked == true && checked[el.getAttribute('type')] ) || ( t0 == 2 && el.selected );
+				case'enabled':return enabled[t0 = el.tagName] && !el.getAttribute('disabled');
+				case'disabled':return enabled[t0 = el.tagName] && el.getAttribute('disabled');
+				case'first-child':case'first-of-type':dir = 1;case'last-child':case'last-of-type':
+					if( ( children = el.parentNode.childNodes ) && ( i = j = children.length ) ){
+						m = 0;
+						if( t1 = first[k] ) tag = el.tagName;
+						while( i-- ){
+							t0 = children[dir ? j - i - 1 : i];
+							if( t0.nodeType == 1 && ( t1 ? tag == t0.tagName : 1 ) ) return !m++ && t0 == el;
+						}
+					}
+					return;
+				case'only-of-type':dir = 1;case'only-child':
+					if( ( children = el.parentNode.childNodes ) && ( i = children.length ) ){
+						m = 0;
+						if( dir ) tag = el.tagName;
+						while( i-- ){
+							t0 = children[i];	
+							if( t0.nodeType == 1 ){
+								if( m++ && ( dir ? tag == t0.tagName : 1 ) ) return;
+								else t1 = t0;
 							}
 						}
+						return el == t1;
 					}
-					return t0[ename] == el;
-				case'nth-of-type':case'nth-last-of-type':
-					if( val == 'n' ) return 1;
-					elTagName = el.tagName, t0 = domData(el),
-					key == 'nth-of-type' ? ( tname = 'DQtimeT', ename = 'DQindexT' ) : ( tname = 'DQtimeTL', ename = 'DQindexTL' );
-					if( !t0[tname] || t0[tname] != elTagName + finder.bsRtime ) setIdx( el.parentNode, key == 'nth-of-type' ? 1 : 0, elTagName );
-					elIdx = t0[ename];
-					return val == 'even' || val == '2n' ? elIdx % 2 == 0 :
-						val == 'odd' || val == '2n+1' ? elIdx % 2 == 1 :
-						elIdx == val;
-				case'only-of-type':
-					op = el.parentNode && el.parentNode.childNodes;
-					if( op && ( j = op.length ) ){
-						i = 0, opIdx = 0, elTagName = el.tagName;
-						while( i < j ){
-							if( op[i].nodeType == 1 && elTagName != 'HTML' && elTagName == op[i].tagName && ++opIdx && (val = op[i]) && opIdx > 1 ) return 0;
-							i++;
+					return;
+				default:
+					if( !( parent = el.parentNode ) || parent.tagName == 'HTML' || !( time = times[k] ) || !( children = parent.childNodes ) || !( j = i = children.length ) ) return 0;
+					tag = el.tagName, data = domData(el), index = indexes[k];
+					if( !data[time] || data[time] != qtime[time] ) data[time] = qtime[time] = TIME, update = 1;
+					switch( k ){
+					case'nth-child':dir = 1;case'nth-last-child':
+						if( v == 'n' ) return 1;
+						if( update ) while( i-- ){
+							t0 = children[i];
+							if( t0.nodeType == 1 ) ( t1 = domData(t0) )[time] = TIME, t1[index] = dir ? i + 1 : j - i;
 						}
-						if( opIdx == 1 && el == val ) return 1;
-					}
-					return 0;
-				case'only-child':
-					pEl = el.parentNode, t0 = domData(el);
-					if( !t0.DQtimeOCH || t0.DQtimeOCH != finder.bsRtime || !t0.DQChElLen ){
-						t0.DQtimeOCH = finder.bsRtime,
-						op = pEl && pEl.childNodes;
-						if( op && ( i = op.length ) ){
-							opIdx = 0, val = null;
+						j = ( i = data[index] ) % 2;
+						return v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : i == v;
+					case'nth-of-type':dir = 1;case'nth-last-of-type':
+						if( v == 'n' ) return 1;
+						if( update ){
+							m = 1;
 							while( i-- ){
-								if( op[i].nodeType == 1 && opIdx++ ) val = op[i];
-								if( opIdx > 2 ) break;
-							}
-							if( opIdx == 1 ) t0.DQChEl = val;
-						}
-						t0.DQChElLen = opIdx;
-					}
-					return t0.DQChElLen == 1 && t0.DQChEl == el;
-				case'first-child':case'last-child':
-					pEl = el.parentNode, t0 = domData(el),
-					key == 'first-child' ? ( tname = 'DQtimeFC', ename = 'DQFCEl' ) : ( tname = 'DQtimeLC', ename = 'DQLCEl' );
-					if( !t0[tname] || t0[tname] != finder.bsRtime ){
-						t0[tname] = finder.bsRtime,
-						op = pEl && pEl.childNodes;
-						if( op && ( j = op.length ) ){
-							t0[ename] = null;
-							if( key.charAt(0) == 'f' ){
-								for( j = i, i = 0 ; i < j ; i++ ){
-									if( op[i].nodeType == 1 ){
-										t0[ename] = op[i];
-										break;
-									}
-								}
-							}else{
-								while( i-- ){
-									if( op[i].nodeType == 1 ){
-										t0[ename] = op[i];
-										break;
-									}
-								}
+								t0 = children[dir ? j - i - 1 : i];
+								if( t0.nodeType == 1 && t0.tagName == tag ) ( t1 = domData(t0) )[time] = TIME, t1[index] = m++;
 							}
 						}
+						j = ( i = data[index] ) % 2;
+						return v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : i == v;
 					}
-					return t0[ename] == el;
-				case'nth-child':
-					t0 = domData(el),
-					key == 'nth-child' ? ( tname = 'DQtime', ename = 'DQindex' ) : ( tname = 'DQtimeL', ename = 'DQindexL' );
-					if( val == 'n' ) return 1;
-					if( !t0[tname] || t0[tname] != finder.bsRtime ) setIdx( el.parentNode, 1 );
-					elIdx = t0[ename];
-					return val == 'even' || val == '2n' ? elIdx % 2 == 0 :
-						val == 'odd' || val == '2n+1' ? elIdx % 2 == 1 :
-						elIdx == val;
-				default:return 0;
+					return token == el.tagName || token == '*';
 				}
-			default:return token == el.tagName || token == '*';
 			}
 		};
 	})();
-	query = function( query, doc, ret ){
-		var el, els, sels, t0, i, j, k, m, n,
-			tags, key, hit, token, tokens, hasQS;
-		if( ret ) ret.length = 0;
-		else ret = [];
-		doc = doc || document,
-		finder.bsRtime = +new Date(),
-		finder.lastQuery = query;
-		if( rClsTagId.test(query) ){
-			if( ( key = query.charAt(0) ) == '#' )
-				return ret[0] = doc.getElementById( query.substr(1) ), ret;
-			else if( key == '.' && isClsName )
-				return doc.getElementsByClassName( query.substr(1) );
-			else if( rTag.test( query ) )
-				return doc.getElementsByTagName( query );
-		}
-		if( isQSA && query.indexOf(',') > -1 && query.indexOf('!') < 0 ) return doc.querySelectorAll( query );
+	query = (function(){
+		var DOC = document, R = {length:0};
+		return function( query, doc, r ){
+			var el, els, sels, t0, i, j, k, m, n,
+				tags, key, hit, token, tokens, hasQS;
+			
+			if( !r ) r = R;
+			r.length = 0, doc = doc || DOC, TIME = +new Date();
+
+			if( rClsTagId.test(query) ) switch( query.charAt(0) ){
+				case'#':return r[r.length++] = doc.getElementById(query.substr(1)), r;
+				case'.':
+					t0 = doc.getElementsByClassName(query.substr(1)), r.length = i = t0.length;
+					while(i--) r[i] = t0[i];
+					return r;
+				default:
+					doc.getElementsByTagName(query);
+					while(i--) r[i] = t0[i];
+					return r;
+			}
+			
+			if( isQSA && query.indexOf(',') > -1 && query.indexOf('!') < 0 ) return doc.querySelectorAll(query);
+
+
+
+
 		oSel.length = 0,
 		hasQS = 0,
 		sels = utrim( query.split(',') );
