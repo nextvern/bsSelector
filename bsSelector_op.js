e9b77390869b18798186dc1e3a16c070d93814cc
hikaMaeng <hika00@gmail.com>
2014-04-29 01:31:57 +0900

    일단 캐쉬제거후 정리, 테스트시작할 정도

--- bsSelector_op.js
+++ bsSelector_op.js
@@ -6,53 +6,13 @@
  * http://css4-selectors.com/browser-selector-test/
  * http://kimblim.dk/css-tests/selectors/
 */
-var bsSelector = function( doc, trim, detect, domData ){
+var bsSelector = function( doc, trim ){
 	'use strict';
-	var isQSA = doc['querySelectorAll'] ? 1 : 0, isClsName = doc['getElementsByClassName'] ? 1 : 0,
-		isIE = detect.browser == 'ie' ? 1 : 0, ieVer = detect.browserVer,
-		rTag = /^[a-z]+[0-9]*$/i, rAlpha = /[a-z]/i, rClsTagId = /^[.#]?[a-z0-9]+$/i,
-		tokenize, compare, query, qtime, TIME, hasParent, hasQSAErr, oSel;
-	
-	
-	oSel = [],
-	tokenize = (function(trim){
-		var mParent = {' ':1, '>':1}, mQSAErr = '!', mBracket = {'[':0, '(':0, ']':1, ')':1},
-			mEx = {'*':1, ' ':1, ']':1, '>':1, '+':1, '~':1, '^':1, '$':1},
-			mT0 = {' ':1, '*':2, '>':2, '+':2, '~':2, '#':3, '.':3, ':':3, '[':3}, mT1 = {'>':1, '+':1, '~':1};
-		return function( s, tks ){
-			var tk, b, t0, t1, i, k;
-			tks.length = 0, tk = '', s = s.replace( trim, '' ), i = s.length;
-			while( i-- ){
-				k = s.charAt(i);
-				if( hasParent || mParent[k] ) hasParent = 1;
-				if( hasQSAErr || b && k == mQSAErr ) hasQSAErr = 1;
-				if( ( t0 = mBracket[k] ) !== undefined && ( b = t0 ) ) continue;
-				if( !( t0 = mEx[k] ) ) tk = k + tk;
-				if( t0 && b ) tk = k + tk;
-				else if( ( t0 = mT0[k] ) == 1 ){
-					if( ( t1 = tks[tks.length - 1] ) == ' ' ) continue;
-					if( tk ) tks[tks.length] = t1 = tk, tk = '';
-					if( !mT1[t1] ) tks[tks.length] = k;
-				}else if( t0 == 2 ){
-					if( tk.replace( trim, '' ) ) tks[tks.length] = tk, tk = '';
-					if( tks[tks.length - 1] == ' ' ) tks.pop();
-					tks[tks.length] = k;
-				}else if( t0 == 3 || !i ){
-					if( tk && !b ) tks[tks.length] = tk, tk = '';
-				}else if( s.charAt( i - 1 ) == ' ' ) tks[tks.length] = tk, tk = '';
-			}
-			return tks;
-		};
-	})(trim),
-	qtime = {},
-	compare = (function(){
+	var compare = (function(){
 		var r0 = /"|'/g, i, j, 
 		mT0 = {'~':1, '|':1, '!':1, '^':1, '$':1, '*':1},
 		enabled = {INPUT:1, BUTTON:1, SELECT:1, OPTION:1, TEXTAREA:1},
-		checked = {INPUT:1, radio:1, checkbox:1, OPTION:2},
-		first = {'first-child':0, 'last-child':0, 'first-of-type':1, 'last-of-type':1},
-		times = {'nth-child':'DQt0', 'nth-last-child':'DQt1', 'first-child':'DQt2', 'last-child':'DQt3', 'first-of-type':'DQt4', 'last-of-type':'DQt5'},
-		indexes = {'nth-child':'DQi0', 'nth-last-child':'DQi1', 'first-child':'DQi2', 'last-child':'DQi3', 'first-of-type':'DQi4', 'last-of-type':'DQi5'};
+		checked = {INPUT:1, radio:1, checkbox:1, OPTION:2};
 		return function( el, token ){
 			var data, time, indexes, parent, children, tag, update, dir, t0, t1, k, v, i, j, m;
 			switch( token.charAt(0) ){
@@ -108,154 +68,174 @@ var bsSelector = function( doc, trim, detect, domData ){
 					}
 					return;
 				default:
-					if( !( parent = el.parentNode ) || parent.tagName == 'HTML' || !( time = times[k] ) || !( children = parent.childNodes ) || !( j = i = children.length ) ) return 0;
-					tag = el.tagName, data = domData(el), index = indexes[k];
-					if( !data[time] || data[time] != qtime[time] ) data[time] = qtime[time] = TIME, update = 1;
+					if( !( parent = el.parentNode ) || parent.tagName == 'HTML' || !( children = parent.childNodes ) || !( j = i = children.length ) ) return;
+					if( v == 'n' ) return 1;
+					t1 = 0;
 					switch( k ){
-					case'nth-child':dir = 1;case'nth-last-child':
-						if( v == 'n' ) return 1;
-						if( update ) while( i-- ){
+					case'nth-child':
+						for( i = 0 ; i < j ; i++ ){
 							t0 = children[i];
-							if( t0.nodeType == 1 ) ( t1 = domData(t0) )[time] = TIME, t1[index] = dir ? i + 1 : j - i;
-						}
-						j = ( i = data[index] ) % 2;
-						return v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : i == v;
-					case'nth-of-type':dir = 1;case'nth-last-of-type':
-						if( v == 'n' ) return 1;
-						if( update ){
-							m = 1;
-							while( i-- ){
-								t0 = children[dir ? j - i - 1 : i];
-								if( t0.nodeType == 1 && t0.tagName == tag ) ( t1 = domData(t0) )[time] = TIME, t1[index] = m++;
+							if( t0.nodeType == 1 ){
+								t1++;
+								if( el == t0 ) return j = t1 % 2, v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : t1 == v;
 							}
 						}
-						j = ( i = data[index] ) % 2;
-						return v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : i == v;
+						return;
+					case'nth-last-child':
+						while( i-- ){
+							t0 = children[i];
+							if( t0.nodeType == 1 ){
+								t1++;
+								if( el == t0 ) return j = t1 % 2, v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : t1 == v;
+							}
+						}
+						return;
+					case'nth-of-type':
+						tag = el.tagName;
+						for( i = 0 ; i < j ; i++ ){
+							t0 = children[i];
+							if( t0.nodeType == 1 && t0.tagName == tag ){
+								t1++;
+								if( el == t0 ) return j = t1 % 2, v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : t1 == v;
+							}
+						}
+						return;
+					case'nth-last-of-type':
+						tag = el.tagName;
+						while( i-- ){
+							t0 = children[i];
+							if( t0.nodeType == 1 && t0.tagName == tag ){
+								t1++;
+								if( el == t0 ) return j = t1 % 2, v == 'even' || v == '2n' ? j == 0 : v == 'odd' || v == '2n+1' ? j == 1 : t1 == v;
+							}
+						}
+						return;
 					}
 					return token == el.tagName || token == '*';
 				}
 			}
 		};
-	})();
-	query = (function(){
-		var DOC = document, R = {length:0};
-		return function( query, doc, r ){
-			var el, els, sels, t0, i, j, k, m, n,
-				tags, key, hit, token, tokens, hasQS;
-			
-			if( !r ) r = R;
-			r.length = 0, doc = doc || DOC, TIME = +new Date();
-
-			if( rClsTagId.test(query) ) switch( query.charAt(0) ){
-				case'#':return r[r.length++] = doc.getElementById(query.substr(1)), r;
-				case'.':
-					t0 = doc.getElementsByClassName(query.substr(1)), r.length = i = t0.length;
-					while(i--) r[i] = t0[i];
-					return r;
-				default:
-					doc.getElementsByTagName(query);
-					while(i--) r[i] = t0[i];
-					return r;
+	})(),
+	isQSA = doc['querySelectorAll'] ? 1 : 0,
+	rTag = /^[a-z]+[0-9]*$/i, rAlpha = /[a-z]/i, rClsTagId = /^[.#]?[a-z0-9]+$/i,
+	DOC = document, tagName = {}, clsName = {},
+	className = (function( tagName, clsName ){
+		var reg = {}, r = {length:0};
+		return document['getElementsByClassName'] ? function(cls){
+			console.log( DOC, DOC.getElementsByClassName(cls), cls );
+			return clsName[cls] || ( clsName[cls] = DOC.getElementsByClassName(cls) );
+		} : function(cls){
+			var t0 = tagName['*'] || ( tagName['*'] = DOC.getElementsByTagName('*') ), t1 = r[cls] || ( r[cls] = new RegExp( '\\b' + cls + '\\b', 'g' ) ), i;
+			r.length = 0;
+			while( i-- ) if( t1.test(t0[i].className) ) r[r.length++] = t0[i];
+			return r;
+		};
+	})( tagName, clsName ),
+	mQSA = {' ':1,'+':1,'~':1,':':1,'[':1},
+	mParent = {' ':1, '>':1}, mQSAErr = '!', mBracket = {'[':1, '(':1, ']':2, ')':2},
+	mEx = {' ':1, '*':1, ']':1, '>':1, '+':1, '~':1, '^':1, '$':1},
+	mT0 = {' ':1, '*':2, '>':2, '+':2, '~':2, '#':3, '.':3, ':':3, '[':3}, mT1 = {'>':1, '+':1, '~':1},
+	R = {length:0}, arrs = {_l:0};
+	
+	return function( query, doc, r ){
+		var sels, sel, 
+			hasParent, hasQSAErr, hasQS,
+			t0, t1, t2, t3, i, j, k, l, m, n,
+			el, els, tags, key, hit, token, tokens;
+		
+		if( !r ) r = R;
+		r.length = 0, doc ? ( DOC = doc ) : ( doc = DOC );
+		if( rClsTagId.test(query) ) switch( query.charAt(0) ){
+			case'#':return r[r.length++] = doc.getElementById(query.substr(1)), r;
+			case'.':return className(query.substr(1));
+			default:return tagName[query] || ( tagName[query] = doc.getElementsByTagName(query) );
+		}
+		//if( isQSA && ( i = query.indexOf(',') ) > -1 && query.indexOf('!') < 0 ) return doc.querySelectorAll(query);
+		if( i == -1 ) sels = arrs._l ? arrs[--arrs._l] : [], sels[0] = query, i = 1;
+		else sels = query.split(','), i = sels.length;
+		while( i-- ){
+			t0 = arrs._l ? arrs[--arrs._l] : [], t1 = '', sel = sels[i].replace( trim, '' ), m = 0, j = sel.length;
+			while( j-- ){
+				k = sel.charAt(j);
+				if( hasParent || mParent[k] ) hasParent = 1;
+				if( hasQSAErr || m == 2 && k == '!' ) hasQSAErr = 1;
+				if( ( t2 = mBracket[k] ) && ( b = t2 ) ) continue;
+				if( !( t2 = mEx[k] ) ) t1 = k + t1;
+				if( t2 && m == 2 ) t1 = k + t1;
+				else if( ( t2 = mT0[k] ) == 1 ){
+					if( ( t3 = t0[t0.length - 1] ) == ' ' ) continue;
+					if( t1 ) t0[t0.length] = t1, t1 = '';
+					if( !mT1[t3] ) t0[t0.length] = k;
+				}else if( t2 == 2 ){
+					if( t1.replace( trim, '' ) ) t0[t0.length] = t1, t1 = '';
+					if( t0[t0.length - 1] == ' ' ) t0.pop();
+					t0[t0.length] = k;
+				}else if( t2 == 3 || !j ){
+					if( t1 && m < 2 ) t0[t0.length] = t1, t1 = '';
+				}else if( sel.charAt( j - 1 ) == ' ' ) t0[t0.length] = t1, t1 = '';
 			}
-			
-			if( isQSA && query.indexOf(',') > -1 && query.indexOf('!') < 0 ) return doc.querySelectorAll(query);
-
-
-
-
-		oSel.length = 0,
-		hasQS = 0,
-		sels = utrim( query.split(',') );
-		for( i = sels.length; i--; ){
-			t0 = parseQuery( sels[i] );
-			for( j = t0.length; j--; ){
-				if( rTag.test( t0[j] ) )
-					t0[j] = t0[j].toUpperCase();
+			j = t0.length;
+			while( j-- ){
+				if( rTag.test(t0[j]) ) t0[j] = t0[j].toUpperCase();
 				else if( t0[j].charAt(0) == ':' ){
 					t0[j] = t0[j].toLowerCase();
 					if( ( t0[j] == ':nth-child(n' || t0[j] == ':nth-last-child(n' ) && t0.length != 1 ){
-						t0.splice(j,1);continue;
+						t0.splice( j, 1 );
+						continue;
 					}
 				}
-				if( isQSA && !hasQSAErr && !hasQS && !nQSA.indexOf( t0[j].charAt(0) ) > -1 ) hasQS = 1;
+				if( isQSA && !hasQSAErr && !hasQS && !mQSA[t0[j].charAt(0)] ) hasQS = 1;
 			}
-			oSel.push( t0 );
+			sels[i] = t0;
 		}
-		//console.log("### oSel", oSel);return;
-		if( oSel.length == 1 && oSel[0].length ){
-			if( ( key = oSel[0][0].charAt(0) ) == '#' ){
-				els = [doc.getElementById( oSel[0][0].substr(1) )],
-				oSel[0].shift();
-			}
-			else if( key == '.' && isClsName ){
-				els = doc.getElementsByClassName( oSel[0][0].substr(1) ),
-				oSel[0].shift();
-				if( hasQS && els.length > 100 ) return doc.querySelectorAll( query );
-			}
-			else if( key == '[' || key == ':' ){
-				if( hasQS ) return doc.querySelectorAll( query );
+		if( sels.length == 1 ){
+			t0 = sels[0][0];
+			if( ( k = t0.charAt(0) ) == '#' ) els = arrs._l ? arrs[--arrs._l] : [], els[0] = doc.getElementById(t0.substr(1)), sels[0].shift();
+			else if( k == '.' ){
+				els =  className(t0.substr(1)), sels[0].shift();
+				//if( hasQS && els.length > 100 ) return doc.querySelectorAll(query);
+			}else if( k == '[' || k == ':' ){
+				if( hasQS ) return doc.querySelectorAll(query);
 				if( !hasParent ){
-					els = oSel[0][oSel[0].length-1];
-					if( ( key = els.charAt(0) ) == '#' )
-						els = [doc.getElementById( els.substr(1) )],
-						oSel[0].pop();
-					else if( key == '.' && isClsName )
-						els = doc.getElementsByClassName( els.substr(1) ),
-						oSel[0].pop();
-					else if( rTag.test( els ) )
-						els = doc.getElementsByTagName( els ),
-						oSel[0].pop();
-					else
-						els = doc.getElementsByTagName('*');
+					t0 = sels[0][sels[0].length - 1], k = t0.charAt(0);
+					if( k == '#' ) sels[0].pop(), els = arrs._l ? arrs[--arrs._l] : [], els[0] = doc.getElementById( t0.substr(1) );
+					else if( k == '.' ) sels[0].pop(), els = doc.getElementsByClassName( t0.substr(1) );
+					else if( rTag.test(t0) ) sels[0].pop(), els = tagName[t0] || ( tagName[t0] = doc.getElementsByTagName(els) );
 				}
-				else
-					els = doc.getElementsByTagName('*');
+			}else if( rTag.test(t0) ){
+				sels[0].shift(), els = tagName[t0] || ( tagName[t0] = doc.getElementsByTagName(els) );
+				//if( hasQS && els.length > 100 ) return doc.querySelectorAll(query);
 			}
-			else if( rTag.test( els = oSel[0][0] ) ){
-				els = doc.getElementsByTagName( els ),
-				oSel[0].shift();
-				if( hasQS && els.length > 100 ) return doc.querySelectorAll( query );
-			}
-			else els = doc.getElementsByTagName('*');
-		}else{
-			els = doc.getElementsByTagName('*');
 		}
-		if( !oSel[0].length ) return els;
-		for( i = 0, j = els.length; i < j; i++ ){
-			for( k = oSel.length; k--; ){
-				tokens = oSel[k];
-				el = els[i];
-				for( m = 0, n = tokens.length; m < n; m++ ){
+		if( !els ) els = tagName['*'] || ( tagName['*'] = doc.getElementsByTagName('*') );
+		if( !sels[0].length ) return arrs[arr._l++] = sels[0], sel.length = 0, arrs[arr._l++] = sel, els;
+		for( i = 0, j = els.length ; i < j ; i++ ){
+			l = sels.length;
+			while( l-- ){
+				for( tokens = sels[l], el = els[i], m = 0, n = tokens.length; m < n; m++ ){
 					token = tokens[m];
-					hit = 0;
-					if( ( key = token.charAt(0) ) == ' ' ){ // loop parent
+					if( k == ' ' ){
 						m++;
-						while( el = el.parentNode ){
-							if( hit = compareEl( el, tokens[m] ) ) break;
-						}
-					}else if( key == '>' ){ // immediate parent
-						hit = compareEl( el = el.parentNode, tokens[++m] );
-					}else if( key == '+' ){ // has immediate nextsibling
+						while( el = el.parentNode ) if( hit = compare( el, token[m] ) ) break;
+					}else if( key == '>' ) hit = compare( el = el.parentNode, tokens[++m] );
+					else if( key == '+' ){
 						while( el = el.previousSibling ) if( el.nodeType == 1 ) break;
-						hit = el && compareEl( el, tokens[++m] );
-					}else if( key == '~' ){ // has any nextsibling
+						hit = el && compare( el, tokens[++m] );
+					}else if( key == '~' ){
 						m++;
-						while( el = el.previousSibling ){
-							if( el.nodeType == 1 && compareEl( el, tokens[m] ) ){
-								hit = 1; break;
-							}
+						while( el = el.previousSibling ) if( el.nodeType == 1 && compare( el, tokens[m] ) ){
+							hit = 1;
+							break;
 						}
-					}else{
-						hit = compareEl( el, token );
-					}
-					if( !hit ) break; // 여긴 AND 연산
+					}else hit = compare( el, token );
+					if( !hit ) break;
 				}
-				if( hit ) break; // 여긴 OR 연산
+				if( i == j - 1 ) tokens.length = 0, arrs[arrs._l++] = tokens;
+				if( hit ) break;
 			}
-			if( hit ) ret[ret.length] = els[i];
+			if( i == j - 1 ) sels.length = 0, arrs[arrs._l++] = sels;
+			if( hit ) r[r.length++] = els[i];
 		}
-		return ret;
+		return r;
 	};
-	query.isQSA = isQSA;
-	return query;
 };
